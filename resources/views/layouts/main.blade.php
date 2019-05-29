feee3c239c55d5ebe86012ea559a532bfd5301b9
Setonator <sethjespinas@gmail.com>
2019-05-29 08:55:56 +0800

    added branding to title

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>@yield('title')</title>
+    <title>PC Trade - @yield('title')</title>
 
     <link rel="stylesheet" type="text/css" href="semantic/dist/semantic.min.css">
     <script
